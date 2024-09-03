35ad0dbe49f7cf7677fac8ecad4d377b25786b58
Joshua <Joshua.lucas.rohde@gmail.com>
2024-09-03 10:45:44 -0600

    added stuff

---
+++ code.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("h1").click(function(){
+      alert("Are you bloody well SERIOUS!?!?!?!?");
+    },
+    function(){
+      alert("Are you bloody well SERIOUS!?!?!?!?");
+    }); 
+  });
